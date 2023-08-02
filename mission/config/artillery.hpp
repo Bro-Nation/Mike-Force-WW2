cbd60689ab2490c20ef342566c74c80f3b074e10
SchmutzigMann <eugeneye@ymail.com>
2023-08-02 14:47:31 -0700

    WIP artillery backpack

--- mission/config/artillery.hpp
+++ mission/config/artillery.hpp
@@ -8,9 +8,9 @@ class vn_artillery_settings
         unit_trait_required = 1;
 	      // Distance from the edge of a blacklisted marker that a artillery/aircraft cannot be called in.
 	      danger_distance = 150;
-        radio_backpacks[] = {"vn_o_pack_t884_01", "vn_o_pack_t884_ish54_01_pl", "vn_o_pack_t884_m1_01_pl", "vn_o_pack_t884_m38_01_pl", "vn_o_pack_t884_ppsh_01_pl", "vn_b_pack_prc77_01_m16_pl", "vn_b_pack_03_m3a1_pl", "vn_b_pack_03_xm177_pl", "vn_b_pack_03_type56_pl", "vn_b_pack_03", "vn_b_pack_prc77_01", "vn_b_pack_trp_04", "vn_b_pack_trp_04_02", "vn_b_pack_03", "vn_b_pack_03_02", "vn_b_pack_lw_06"};
+        radio_backpacks[] = {"vn_o_pack_t884_01", "B_SPE_US_Radio_alt", "B_SPE_US_Radio", "B_SPE_US_Radio_packboard_light", "B_SPE_US_Radio_packboard", "vn_o_pack_t884_ish54_01_pl", "vn_o_pack_t884_m1_01_pl", "vn_o_pack_t884_m38_01_pl", "vn_o_pack_t884_ppsh_01_pl", "vn_b_pack_prc77_01_m16_pl", "vn_b_pack_03_m3a1_pl", "vn_b_pack_03_xm177_pl", "vn_b_pack_03_type56_pl", "vn_b_pack_03", "vn_b_pack_prc77_01", "vn_b_pack_trp_04", "vn_b_pack_trp_04_02", "vn_b_pack_03", "vn_b_pack_03_02", "vn_b_pack_lw_06"};
         radio_vehicles[] = {"vn_b_boat_05_01", "vn_b_wheeled_m54_03", "vn_b_wheeled_m151_01", "vn_b_wheeled_m54_02", "vn_b_wheeled_m54_01", "vn_b_wheeled_m54_mg_02", "vn_i_air_ch34_02_01", "vn_i_air_ch34_01_02", "vn_i_air_ch34_02_02"};
-        player_types[] = {"vn_b_men_sog_05", "vn_b_men_sog_17", "vn_b_men_army_08", "vn_o_men_nva_dc_13", "vn_o_men_nva_65_27", "vn_o_men_nva_65_13", "vn_o_men_nva_27", "vn_o_men_nva_13", "vn_o_men_nva_marine_13", "vn_o_men_nva_navy_13", "vn_o_men_vc_local_27", "vn_o_men_vc_local_13", "vn_o_men_vc_regional_13"};
+        player_types[] = {"vn_b_men_sog_05", "vn_b_men_sog_17", "vn_b_men_sf_06", "vn_b_men_army_08", "vn_o_men_nva_dc_13", "vn_o_men_nva_65_27", "vn_o_men_nva_65_13", "vn_o_men_nva_27", "vn_o_men_nva_13", "vn_o_men_nva_marine_13", "vn_o_men_nva_navy_13", "vn_o_men_vc_local_27", "vn_o_men_vc_local_13", "vn_o_men_vc_regional_13"};
         // Planes
         class aircraft
         {
@@ -39,11 +39,11 @@ class vn_artillery_settings
                         };
                         class rambler
                         {
-				                        displayname = $STR_VN_ARTILLERY_AIRCRAFT_CLUSTER_RAMBLER_NAME;
+				displayname = $STR_VN_ARTILLERY_AIRCRAFT_CLUSTER_RAMBLER_NAME;
                                 icon = "vn\ui_f_vietnam\data\decals\vn_callsign_src_433tfs_ca.paa";
                                 description = $STR_VN_ARTILLERY_AIRCRAFT_HE_VESPA_DESCRIPTION;
                                 magazines[] = {"vn_bomb_500_mk82_he_mag_x1","vn_bomb_500_mk82_he_mag_x1"};
-                                vehicleclass = "vn_b_air_f4c_cas";
+                                vehicleclass = "SPE_P47";
                                 cooldown = (60*5);
                                 cost = 10;
                         };
@@ -53,7 +53,7 @@ class vn_artillery_settings
                                 icon = "vn\ui_f_vietnam\data\decals\vn_callsign_src_vf111_ca.paa";
                                 description = $STR_VN_ARTILLERY_AIRCRAFT_HE_SUNDOWNER_DESCRIPTION;
                                 magazines[] = {"vn_bomb_500_blu1b_fb_mag_x1", "vn_bomb_500_blu1b_fb_mag_x1"};
-                                vehicleclass = "vn_b_air_f4c_cas";
+                                vehicleclass = "SPE_P47";
                                 allow_double = 1;
                                 cooldown = (60*5);
                                 cost = 15;
@@ -64,7 +64,7 @@ class vn_artillery_settings
                                 icon = "vn\ui_f_vietnam\data\decals\vn_callsign_src_vmfa323_ca.paa";
                                 description = $STR_VN_ARTILLERY_AIRCRAFT_HE_SNAKE_DESCRIPTION;
                                 magazines[] = {"vn_rocket_s5_he_x16"};
-                                vehicleclass = "vn_b_air_f4c_cas";
+                                vehicleclass = "SPE_P47";
                                 cooldown = (60*5);
                                 cost = 10;
                         };
@@ -74,7 +74,7 @@ class vn_artillery_settings
                                 icon = "vn\ui_f_vietnam\data\decals\vn_callsign_src_vf96_ca.paa";
                                 description = $STR_VN_ARTILLERY_AIRCRAFT_HE_SHOWTIME_DESCRIPTION;
                                 magazines[] = {"vn_m61a1"};
-                                vehicleclass = "vn_b_air_f4c_cas";
+                                vehicleclass = "SPE_P47";
                                 cooldown = (60*5);
                                 cost = 6;
                         };
@@ -84,7 +84,7 @@ class vn_artillery_settings
                                 icon = "vn\ui_f_vietnam\data\decals\vn_callsign_src_1sos_ca.paa";
                                 description = $STR_VN_ARTILLERY_AIRCRAFT_HE_HOBO_DESCRIPTION;
                                 magazines[] = {"vn_m61a1"};
-                                vehicleclass = "vn_b_air_f4c_cas";
+                                vehicleclass = "SPE_P47";
                                 cooldown = (60*5);
                                 cost = 6;
                         };
@@ -94,7 +94,7 @@ class vn_artillery_settings
                                 icon = "vn\ui_f_vietnam\data\decals\vn_callsign_src_b101_ca";
                                 description = $STR_VN_ARTILLERY_AIRCRAFT_HE_CONDOR_DESCRIPTION;
                                 magazines[] = {"vn_rocket_ffar_m229_he_x7", "vn_rocket_ffar_m229_he_x7"};
-                                vehicleclass = "vn_b_air_ah1g_01";
+                                vehicleclass = "SPE_P47";
                                 cooldown = (5*60);
                                 cost = 6;
                         };
@@ -104,7 +104,7 @@ class vn_artillery_settings
                                 icon = "vn\ui_f_vietnam\data\decals\vn_callsign_src_a477_ca.paa";
                                 description = $STR_VN_ARTILLERY_AIRCRAFT_HE_DRAGON_DESCRIPTION;
                                 magazines[] = {"vn_rocket_ffar_m229_he_x19", "vn_rocket_ffar_m229_he_x19"};
-                                vehicleclass = "vn_b_air_uh1c_01_02";
+                                vehicleclass = "SPE_P47";
                                 cooldown = (5*60);
                                 cost = 16;
                         };
@@ -118,7 +118,7 @@ class vn_artillery_settings
                                 icon = "vn\ui_f_vietnam\data\decals\vn_callsign_src_433tfs_ca.paa";
                                 description = $STR_VN_ARTILLERY_AIRCRAFT_CLUSTER_RAMBLER_DESCRIPTION;
                                 magazines[] = {"vn_bomb_f4_out_500_mk20_cb_mag_x1", "vn_bomb_f4_out_500_mk20_cb_mag_x1"};
-                                vehicleclass = "vn_b_air_f4c_cas";
+                                vehicleclass = "SPE_P47";
                                 allow_double = 1;
                                 cost = 20;
                         };
@@ -132,7 +132,7 @@ class vn_artillery_settings
                                 icon = "vn\ui_f_vietnam\data\decals\vn_callsign_src_vmfa314_ca.paa";
                                 description = $STR_VN_ARTILLERY_AIRCRAFT_FLECHETTE_COMBAT_DESCRIPTION;
                                 magazines[] = {"","","vn_rocket_ffar_f4_lau3_wdu4_flechette_x19","vn_rocket_ffar_f4_lau3_wdu4_flechette_x19"};
-                                vehicleclass = "vn_b_air_f4c_cas";
+                                vehicleclass = "SPE_P47";
                                 allow_double = 1;
                                 cooldown = (5*60);
                                 cost = 8;
@@ -143,7 +143,7 @@ class vn_artillery_settings
                                 icon = "vn\ui_f_vietnam\data\decals\vn_callsign_src_29tas_ca.paa";
                                 description = $STR_VN_ARTILLERY_AIRCRAFT_FLECHETTE_BANSHEE_DESCRIPTION;
                                 magazines[] = {"vn_rocket_ffar_wdu4_flechette_x7","vn_rocket_ffar_wdu4_flechette_x7"};
-                                vehicleclass = "vn_b_air_ah1g_04";
+                                vehicleclass = "SPE_P47";
                                 allow_double = 1;
                                 cooldown = (5*60);
                                 cost = 6;
@@ -154,7 +154,7 @@ class vn_artillery_settings
                                 icon = "vn\ui_f_vietnam\data\decals\vn_callsign_src_vmo3_co.paa";
                                 description = $STR_VN_ARTILLERY_AIRCRAFT_FLECHETTE_SCARFACE_DESCRIPTION;
                                 magazines[] = {"vn_rocket_ffar_wdu4_flechette_x19","vn_rocket_ffar_wdu4_flechette_x19"};
-                                vehicleclass = "vn_b_air_uh1c_01_07";
+                                vehicleclass = "SPE_P47";
                                 allow_double = 1;
                                 cooldown = (5*60);
                                 cost = 12;
@@ -169,38 +169,10 @@ class vn_artillery_settings
                                 icon = "vn\ui_f_vietnam\data\decals\vn_callsign_src_hml367_ca.paa";
                                 description = $STR_VN_ARTILLERY_AIRCRAFT_HE_EAGLE_CLAW_DESCRIPTION;
                                 magazines[] = {"vn_rocket_ffar_m229_he_x19","vn_rocket_ffar_m229_he_x19"};
-                                vehicleclass = "vn_b_air_ah1g_01";
+                                vehicleclass = "SPE_P47";
                                 allow_double = 1;
                                 cooldown = (5*60);
                                 cost = 6;
-                        };
-                };
-                class illumination
-                {
-                        displayname = $STR_VN_ARTILLERY_AIRCRAFT_ILLUMINATION_ILLUMINATION_NAME;
-                        class gnat
-                        {
-                                displayname = $STR_VN_ARTILLERY_AIRCRAFT_ILLUMINATION_GNAT_NAME;
-                                icon = "vn\ui_f_vietnam\data\decals\vn_callsign_src_a101_ca.paa";
-                                description = $STR_VN_ARTILLERY_AIRCRAFT_ILLUMINATION_GNAT_DESCRIPTION;
-                                magazines[] = {};
-                                vehicleclass = "vn_b_air_uh1d_02_03";
-                                allow_double = 1;
-                                cooldown = (5*60);
-                                illumination = 1;
-                                cost = 0;
-                        };
-                class dawn_1
-                {
-                                displayname = $STR_VN_ARTILLERY_ARTILLERY_ILLUMINATION_DAWN_1_NAME;
-                                icon = "vn\ui_f_vietnam\data\decals\vn_callsign_src_usarmy_ca.paa";
-                                description = $STR_VN_ARTILLERY_ARTILLERY_ILLUMINATION_DAWN_1_DESCRIPTION;
-                                condition = "daytime >= 18 || daytime <= 6";
-                                function = "vn_fnc_artillery_dawn_1";
-                                allow_double = 0;
-                                cooldown = (60*5);
-                                illumination = 1;
-                                cost = 0;
                         };
                 };
         };
